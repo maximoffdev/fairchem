7624631ebb8bd7cb9480f372ff42e9410d0383b1
Pascal Stephan <pascal.stephan@gmx.de>
2026-04-13 12:45:18 +0200

    Small fixes for Mu(A) training run

--- src/fairchem/core/models/uma/escn_md.py
+++ src/fairchem/core/models/uma/escn_md.py
@@ -1158,7 +1158,7 @@ class MLP_Dipole_Vector_Head(nn.Module, HeadInterface):
     def __init__(self, backbone: eSCNMDBackbone, property_name: str) -> None:
         super().__init__()
         #head should fit to Mu(A) and Mu_Intra(A)
-        self.property_name = backbone.property_name
+        self.property_name = property_name
         self.sphere_channels = backbone.sphere_channels
         # SO3_Linear transforms sphere channel, lmax=1 features to vector outputs 
         # from sphere channels 1 output vector per atom
